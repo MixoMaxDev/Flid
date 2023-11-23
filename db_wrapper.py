6334b2e714786fe68e3e3d74ec35d708774d8a9c
Linus Horn <linushorn@gmx.net>
2023-11-23 22:26:45 +0100

    working auth system, most apis working

--- db_wrapper.py
+++ db_wrapper.py
@@ -1,270 +1,127 @@
 import sqlite3
-import hashlib
-import time
 
-#Splid clone
-# the way it works is:
-# 1 person pays something for a group of people
-# the group of people then pay the person back
-# we keep track of who owes who what
-
-class User:
-    def __init__(self, name, password) -> None:
-        self.name = name #unique identifier
-        self.owes = {} # {user: amount} means user owes me amount
-        self.owed = {} # {user: amount} means i owe user amount
-        self.balance = 0
-        
-        self.is_authenticated = True #for testing purposes always true
-    
-    def check_auth(self, password) -> bool:
-        return self.password == hashlib.sha256(password.encode()).hexdigest()
-    
-    def __str__(self) -> str:
-        return self.name
-    
-    def add_owes(self, owe: "Owes"):
-        amount = owe.get_amount(self.name)
-        self.owes[owe.sender] = amount
-        self.balance -= amount
-    
-    def add_owed(self, owe: "Owes"):
-        for user, amount_owed in owe.amounts.items():
-            if user == self.name:
-                continue
-            self.owed[user] = amount_owed
-        self.balance += owe.total
-    
-    def print_all_transactions(self):
-        print(f"{self.name} owes:")
-        for user, amount in self.owes.items():
-            print(f"{user}: {amount}")
-        print(f"{self.name} is owed:")
-        for user, amount in self.owed.items():
-            print(f"{user}: {amount}")
-        print(f"{self.name} has a balance of {self.balance}")
-    
-    def update(self):
-        #get all transactions from db
-        transactions = db.get_owes(sender=self.name)
-        for transaction in transactions:
-            #transaction: (id, sender, receivers, amounts)
-            receivers = transaction[2].split(",")
-            amounts = transaction[3].split(",")
-            amounts = {receivers[i]: float(amounts[i]) for i in range(len(receivers))}
-            owe = Owes(transaction[1], amounts)
-            self.add_owes(owe)
-        
-        transactions = db.get_owes(receiver=self.name)
-        for transaction in transactions:
-            #transaction: (id, sender, receivers, amounts)
-            receivers = transaction[2].split(",")
-            amounts = transaction[3].split(",")
-            amounts = {receivers[i]: float(amounts[i]) for i in range(len(receivers))}
-            owe = Owes(transaction[1], amounts)
-            self.add_owed(owe)
-
-#---
-
-class Owes:
-    def __init__(self, sender, amounts):
-        self.sender = sender
-        self.amounts: dict[User, float] = amounts # {user: amount}
-        self.total = sum(amounts.values())
-    
-    def get_amount(self, user:str) -> float:
-        return self.amounts.get(user, 0)
-
-#---
-
-class DataBase:
+class DBWrapper:
     def __init__(self) -> None:
-        self.path = "./data/data.db"
-        self.users = {} # {name: User}
-        self.owes = []
-
+        self.path = "./data/database.db"
         self.conn = sqlite3.connect(self.path)
         self.cursor = self.conn.cursor()
         
-        self.create_tables_if_not_exists()
+        self.create_tables()
     
-    def create_tables_if_not_exists(self):
-        cmd = """
-        CREATE TABLE IF NOT EXISTS users (
-            name TEXT PRIMARY KEY,
-            password TEXT NOT NULL
-        );
-        """
-        self.cursor.execute(cmd)
-        cmd = """
-        CREATE TABLE IF NOT EXISTS owes (
+    def create_tables(self):
+        cmd = """CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            sender TEXT NOT NULL,
-            receivers TEXT NOT NULL,
-            amounts TEXT NOT NULL
-        );
-        """
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            email TEXT NOT NULL
+        );"""
+        self.cursor.execute(cmd)
+        self.conn.commit()
+        
+        cmd = """CREATE TABLE IF NOT EXISTS owes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sender_id INTEGER NOT NULL,
+            receiver_id INTEGER NOT NULL,
+            amount REAL NOT NULL,
+            description TEXT NOT NULL,
+            FOREIGN KEY (sender_id) REFERENCES users (id),
+            FOREIGN KEY (receiver_id) REFERENCES users (id)
+        );"""
         self.cursor.execute(cmd)
         self.conn.commit()
     
-    def add_user(self, name, password):
-        cmd = """
-        INSERT INTO users (name, password) VALUES (?, ?);
-        """
-        self.cursor.execute(cmd, (name, password))
+    def add_user(self, username, password, email):
+        #check if username already exists
+        cmd = """SELECT * FROM users WHERE username = ?;"""
+        self.cursor.execute(cmd, (username,))
+        if self.cursor.fetchone() is not None:
+            return False
+        
+        cmd = """SELECT * FROM users WHERE email = ?;"""
+        self.cursor.execute(cmd, (email,))
+        if self.cursor.fetchone() is not None:
+            return False
+        
+        cmd = """INSERT INTO users (username, password, email) VALUES (?, ?, ?);"""
+        self.cursor.execute(cmd, (username, password, email))
+        self.conn.commit()
+        return True
+    
+    def change_password(self, user_id, new_password):
+        cmd = """UPDATE users SET password = ? WHERE id = ?;"""
+        self.cursor.execute(cmd, (new_password, user_id))
         self.conn.commit()
     
-    def add_owe(self, sender, receivers, amounts):
-        cmd = """
-        INSERT INTO owes (sender, receivers, amounts) VALUES (?, ?, ?);
-        """
-        self.cursor.execute(cmd, (sender, receivers, amounts))
+    def change_email(self, user_id, new_email):
+        cmd = """UPDATE users SET email = ? WHERE id = ?;"""
+        self.cursor.execute(cmd, (new_email, user_id))
         self.conn.commit()
     
-    def update(self, verbose=False):
-        t_start = time.time()
-        cmd = "SELECT * FROM users;"
+    def delete_user(self, user_id):
+        cmd = """DELETE FROM users WHERE id = ?;"""
+        self.cursor.execute(cmd, (user_id,))
+        self.conn.commit()
+    
+    def get_user_by_username(self, username) -> tuple[int, str, str, str]: #id, username, password, email
+        cmd = """SELECT * FROM users WHERE username = ?;"""
+        self.cursor.execute(cmd, (username,))
+        return self.cursor.fetchone()
+    
+    def get_user_by_email(self, email):
+        cmd = """SELECT * FROM users WHERE email = ?;"""
+        self.cursor.execute(cmd, (email,))
+        return self.cursor.fetchone()
+    
+    def get_user_by_id(self, id):
+        cmd = """SELECT * FROM users WHERE id = ?;"""
+        self.cursor.execute(cmd, (id,))
+        return self.cursor.fetchone()
+    
+    def get_all_users(self):
+        cmd = """SELECT * FROM users;"""
         self.cursor.execute(cmd)
-        users = self.cursor.fetchall()
-        self.users = {}
-        for user in users:
-            self.users[user[0]] = User(user[0], user[1])
-        
-        if verbose:
-            print(f"Users loaded in {time_taken(t_start)}")
-        
-        t_start_2 = time.time()
-        cmd = "SELECT * FROM owes;"
+        return self.cursor.fetchall()
+    
+    def get_num_users(self):
+        cmd = "SELECT MAX(id) FROM users"
         self.cursor.execute(cmd)
-        owes = self.cursor.fetchall()
-        self.owes = []
-        for owe in owes:
-            sender = owe[1]
-            receivers = owe[2].split(",")
-            amounts = owe[3].split(",")
-            amounts = {receivers[i]: float(amounts[i]) for i in range(len(receivers))}
-            self.owes.append(Owes(sender, amounts))
-        
-        if verbose:
-            print(f"Owes loaded in {time_taken(t_start_2)}")
-            print(f"Total time taken: {time_taken(t_start)}")
-            
+        return self.cursor.fetchone()[0]
     
-    def get_user_by_name(self, name):
-        cmd = """
-        SELECT * FROM users WHERE name = ?;
-        """
-        self.cursor.execute(cmd, (name,))
-        user = self.cursor.fetchone()
-        if user is None:
-            return None
-        return User(user[0], user[1])
     
-    def get_owes(self, **kwargs):
-        #kwargs: sender, receiver, amount
-        
-        #if all of them are None, return None
-        #if only one of them is valid, search db for that
-        #if more than one is valid, search db for OR and for AND
-
-        valid_dict = {
-        }
-        if "sender" in kwargs:
-            valid_dict["sender"] = kwargs["sender"]
-        if "receiver" in kwargs:
-            valid_dict["receiver"] = kwargs["receiver"]
-        if "amount" in kwargs:
-            valid_dict["amount"] = kwargs["amount"]
-        
-        match len(valid_dict):
-            case 0:
-                cmd = "SELECT * FROM owes;"
-                self.cursor.execute(cmd)
-                return self.cursor.fetchall()
-            case 1:
-                cmd = f"SELECT * FROM owes WHERE {list(valid_dict.keys())[0]} = ?;"
-                self.cursor.execute(cmd, (list(valid_dict.values())[0],))
-                return self.cursor.fetchall()
-            case 2:
-                #return dict: {OR: [list of results], AND: [list of results]}
-                cmd = f"SELECT * FROM owes WHERE {list(valid_dict.keys())[0]} = ? OR {list(valid_dict.keys())[1]} = ?;"
-                self.cursor.execute(cmd, (list(valid_dict.values())[0], list(valid_dict.values())[1]))
-                r_dict = {"OR": self.cursor.fetchall()}
-                cmd = f"SELECT * FROM owes WHERE {list(valid_dict.keys())[0]} = ? AND {list(valid_dict.keys())[1]} = ?;"
-                self.cursor.execute(cmd, (list(valid_dict.values())[0], list(valid_dict.values())[1]))
-                r_dict["AND"] = self.cursor.fetchall()
-                return r_dict
-            case 3:
-                #return dict: {OR: [list of results], AND: [list of results]}
-                cmd = f"SELECT * FROM owes WHERE {list(valid_dict.keys())[0]} = ? OR {list(valid_dict.keys())[1]} = ? OR {list(valid_dict.keys())[2]} = ?;"
-                self.cursor.execute(cmd, (list(valid_dict.values())[0], list(valid_dict.values())[1], list(valid_dict.values())[2]))
-                r_dict = {"OR": self.cursor.fetchall()}
-                cmd = f"SELECT * FROM owes WHERE {list(valid_dict.keys())[0]} = ? AND {list(valid_dict.keys())[1]} = ? AND {list(valid_dict.keys())[2]} = ?;"
-                self.cursor.execute(cmd, (list(valid_dict.values())[0], list(valid_dict.values())[1], list(valid_dict.values())[2]))
-                r_dict["AND"] = self.cursor.fetchall()
-                return r_dict
-
-def time_taken(t1:float) -> str:
-    #beatufully format time taken
-    t2 = time.time()
-    t_delta = t2 - t1
-    #adjust for time unit
-    if t_delta < 1:
-        t_delta *= 1000
-        unit = "ms"
-    elif t_delta > 60:
-        t_delta /= 60
-        unit = "min"
-    elif t_delta > 3600:
-        t_delta /= 3600
-        unit = "hr"
-    elif t_delta > 86400:
-        t_delta /= 86400
-        unit = "day"
-    elif t_delta > 604800:
-        t_delta /= 604800
-        unit = "week"
-    elif t_delta > 2628000:
-        t_delta /= 2628000
-        unit = "month"
-    elif t_delta > 31536000:
-        t_delta /= 31536000
-        unit = "year"
-    else:
-        unit = "s"
     
-    return f"{t_delta:.2f} {unit}"
+    # Owes functions
+    
+    def get_owes_by_sender_id(self, sender_id):
+        cmd = """SELECT * FROM owes WHERE sender_id = ?;"""
+        self.cursor.execute(cmd, (sender_id,))
+        return self.cursor.fetchall()
+    
+    def get_owes_by_receiver_id(self, receiver_id) -> list[tuple[int, int, int, float, str]]: #id, sender_id, receiver_id, amount, description
+        cmd = """SELECT * FROM owes WHERE receiver_id = ?;"""
+        self.cursor.execute(cmd, (receiver_id,))
+        return self.cursor.fetchall()
+    
+    def add_owe(self, sender_id, receiver_id, amount, description):
+        cmd = """INSERT INTO owes (sender_id, receiver_id, amount, description) VALUES (?, ?, ?, ?);"""
+        self.cursor.execute(cmd, (sender_id, receiver_id, amount, description))
+        self.conn.commit()
+    
+    def delete_owe(self, id):
+        cmd = """DELETE FROM owes WHERE id = ?;"""
+        self.cursor.execute(cmd, (id,))
+        self.conn.commit()
 
-#---
+    def get_num_owes(self):
+        cmd = "SELECT MAX(id) FROM owes"
+        self.cursor.execute(cmd)
+        return self.cursor.fetchone()[0]
+    
+    def get_sum_owes(self):
+        cmd = "SELECT SUM(amount) FROM owes"
+        self.cursor.execute(cmd)
+        return self.cursor.fetchone()[0]
 
-#generate random users and data
-import random
-import string
 
-db = DataBase()
-
-db.update(verbose=True)
-
-users = db.users
-usernames = list(users.keys())
-
-num_transactions = 100000
-for _ in range(num_transactions):
-    t_start = time.time()
-    sender = random.choice(usernames)
-    receivers = random.choices(usernames, k=random.randint(1, 10))
-    amounts = [random.randint(1, 100) for _ in range(len(receivers))]
-    db.add_owe(sender, ",".join(receivers), ",".join(map(str, amounts)))
-    percentage = _ / num_transactions * 100
-    print(f"{_} / {num_transactions} ({percentage:.2f}%) in {time_taken(t_start)}")
-
-db.update(verbose=True)
-
-#select 10 random users
-
-users = random.choices(list(db.users.values()), k=10)
-
-for user in users:
-    print(user)
-    user.print_all_transactions()
-    print()
+if __name__ == "__main__":
+    print("This script is not meant to be run directly.")
+    print("Please run the main.py script instead.")
--- main.py
+++ main.py
@@ -1,13 +1,273 @@
-#built in modules
-import time
+import hashlib
 import random
+import time
 
-# third party modules
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.requests import Request
+
 import uvicorn
 
-# import as alias
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
-from fastapi.staticfiles import StaticFiles
+from db_wrapper import DBWrapper
 
-# local modules
+
+
+app = FastAPI()
+db = DBWrapper()
+
+
+# Auth functions
+
+#login info: username / email + password
+#get user by username / email
+#hashed password is stored in db
+#check if password matches
+#return a temporary token
+#token is stored in client cookie and server cache
+#for every request, check if token is valid for that user
+
+def hash(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
+
+class TokenCache:
+    def __init__(self):
+        self.tokens = {} #user_id: [token, creation_time]
+    
+    def _generate_token(self) -> str:
+        token_length = 128
+        token = ""
+        for _ in range(token_length):
+            token += chr(random.randint(0, 255))
+        
+        #hash token
+        token: str = hash(token)
+        return token
+    
+    def add_token(self, user_id: int) -> str:
+        token = self._generate_token()
+        self.tokens[user_id] = [token, int(time.time())]
+        return token
+    
+    def check_token(self, user_id: int, token: str) -> bool:
+        if user_id not in self.tokens:
+            return False
+        
+        server_token, creation_time = self.tokens[user_id]
+        if token != server_token:
+            return False
+        elif int(time.time()) - creation_time > 3600: #token expires after 1 hour
+            return False
+        
+        return True #token is the same and not expired
+
+    def delete_token(self, user_id: int) -> None:
+        del self.tokens[user_id]
+
+token_cache = TokenCache()
+
+@app.get("/auth/login")
+async def login(user_identification: str, password: str) -> JSONResponse:
+    if "@" in user_identification:
+        user = db.get_user_by_email(user_identification)
+    else:
+        user = db.get_user_by_username(user_identification)
+    
+    if user is None:
+        return JSONResponse(status_code=404, content={"error": "User does not exist"})
+    
+    id, username, hashed_password, email = user
+    
+    if hash(password) != hashed_password:
+        return JSONResponse(status_code=401, content={"error": "Incorrect password"})
+    else:
+        token_cache.add_token(id)
+
+        return JSONResponse(status_code=200, content={"token": token_cache.tokens[id][0], "user_id": id})
+
+@app.get("/auth/logout")
+async def logout(user_id: int, token: str) -> JSONResponse:
+    if token_cache.check_token(user_id, token):
+        token_cache.delete_token(user_id)
+        return JSONResponse(status_code=200, content={"success": "Logged out"})
+    else:
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+
+@app.get("/auth/register")
+async def register(username: str, password: str, email: str) -> JSONResponse:
+    if db.add_user(username, hash(password), email):
+        return JSONResponse(status_code=200, content={"success": "User created"})
+    else:
+        return JSONResponse(status_code=400, content={"error": "User already exists"})
+
+@app.get("/auth/check")
+async def check(user_id: int, token: str) -> JSONResponse:
+    if token_cache.check_token(user_id, token):
+        return JSONResponse(status_code=200, content={"success": "Token is valid"})
+    else:
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+
+
+
+# user functions
+
+## public functions (no auth)
+
+@app.get("/user/get")
+async def get_user(user_id: int) -> JSONResponse:
+    user = db.get_user_by_id(user_id)
+    if user is None:
+        return JSONResponse(status_code=404, content={"error": "User does not exist"})
+    else:
+        id, username, password, email = user
+        return JSONResponse(status_code=200, content={"id": id, "username": username, "email": email})
+
+@app.get("/user/get/all")
+async def get_all_users() -> JSONResponse:
+    users = db.get_all_users()
+    users = [{"id": user[0], "username": user[1], "email": user[3]} for user in users]
+    return JSONResponse(status_code=200, content={"users": users})
+
+@app.get("/user/get/num")
+async def get_num_users() -> JSONResponse:
+    num_users = db.get_num_users()
+    return JSONResponse(status_code=200, content={"num_users": num_users})
+
+
+## private functions (auth)
+@app.get("/user/me")
+async def get_me(user_id: int, token: str) -> JSONResponse:
+    if not token_cache.check_token(user_id, token):
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+    
+    user = db.get_user_by_id(user_id)
+    if user is None:
+        return JSONResponse(status_code=404, content={"error": "User does not exist"})
+    else:
+        id, username, password, email = user
+        return JSONResponse(status_code=200, content={"id": id, "username": username, "email": email})
+
+@app.get("/user/me/change/password")
+async def change_password(user_id: int, token: str, new_password: str) -> JSONResponse:
+    if not token_cache.check_token(user_id, token):
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+    
+    db.change_password(user_id, hash(new_password))
+    return JSONResponse(status_code=200, content={"success": "Password changed"})
+
+@app.get("/user/me/change/email")
+async def change_email(user_id: int, token: str, new_email: str) -> JSONResponse:
+    if not token_cache.check_token(user_id, token):
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+    
+    db.change_email(user_id, new_email)
+    return JSONResponse(status_code=200, content={"success": "Email changed"})
+
+@app.get("/user/me/delete")
+async def delete_user(user_id: int, token: str) -> JSONResponse:
+    if not token_cache.check_token(user_id, token):
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+    
+    db.delete_user(user_id)
+    return JSONResponse(status_code=200, content={"success": "User deleted"})
+
+@app.get("/user/me/transactions")
+async def get_transactions(user_id: int, token: str) -> JSONResponse:
+    if not token_cache.check_token(user_id, token):
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+    
+    owes = db.get_owes_by_sender_id(user_id)
+    owed = db.get_owes_by_receiver_id(user_id)
+    
+    total_sum = 0
+    for owe in owes:
+        total_sum += owe[2]
+    for owe in owed:
+        total_sum -= owe[2]
+    
+    return JSONResponse(status_code=200, content={"owes": owes, "owed": owed, "total_sum": total_sum})
+
+@app.get("/user/me/settle")
+async def settle(user_id: int, token: str) -> JSONResponse:
+    if not token_cache.check_token(user_id, token):
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+    
+    #get all transactions
+    #calculate the sum for each user we owe / owe us
+    #if the sum is negative, we owe them
+    #if the sum is positive, they owe us
+    
+    owes = db.get_owes_by_sender_id(user_id)
+    owed = db.get_owes_by_receiver_id(user_id)
+    
+    user_dict = {} #user_id: sum
+    
+    for owe in owes:
+        id, sender_id, receiver_id, amount, description = owe
+        if receiver_id not in user_dict:
+            user_dict[receiver_id] = 0
+        user_dict[receiver_id] += amount
+    
+    for owe in owed:
+        id, sender_id, receiver_id, amount, description = owe
+        if sender_id not in user_dict:
+            user_dict[sender_id] = 0
+        user_dict[sender_id] -= amount
+    
+    owes = {}
+    owed = {}
+    
+    for user_id, user_sum in user_dict.items():
+        if user_sum < 0:
+            owes[user_id] = user_sum
+        elif user_sum > 0:
+            owed[user_id] = user_sum
+    
+    return JSONResponse(status_code=200, content={"owes": owes, "owed": owed})
+
+# owes functions
+
+## public functions (no auth)
+
+@app.get("/owes/get/num")
+async def get_num_owes() -> JSONResponse:
+    num_owes = db.get_num_owes()
+    return JSONResponse(status_code=200, content={"num_owes": num_owes})
+
+@app.get("/owes/get/sum")
+async def get_sum_owes() -> JSONResponse:
+    sum_owes = db.get_sum_owes()
+    return JSONResponse(status_code=200, content={"sum_owes": sum_owes})
+
+
+## private functions (auth)
+
+@app.get("/owes/add")
+async def add_owe(user_id: int, token: str, receiver_id: int, amount: float, description: str) -> JSONResponse:
+    if not token_cache.check_token(user_id, token):
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+    
+    db.add_owe(user_id, receiver_id, amount, description)
+    return JSONResponse(status_code=200, content={"success": "Owe added"})
+
+@app.get("/owes/delete")
+async def delete_owe(user_id: int, token: str, owe_id: int) -> JSONResponse:
+    if not token_cache.check_token(user_id, token):
+        return JSONResponse(status_code=401, content={"error": "Token is not valid"})
+    
+    db.delete_owe(owe_id)
+    return JSONResponse(status_code=200, content={"success": "Owe deleted"})
+
+
+#html
+@app.get("/")
+async def login_page() -> HTMLResponse:
+    return HTMLResponse(content=open("login.html").read(), status_code=200)
+
+@app.get("/home")
+async def home_page() -> HTMLResponse:
+    return HTMLResponse(content=open("home.html").read(), status_code=200)
+
+
+
+uvicorn.run(app, host="127.0.0.1", port=80)
